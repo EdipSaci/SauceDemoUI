d78449b3f75a66bd28308d1b56911c3fe7d16bfb
edip <saciedip@gmail.com>
2023-09-05 12:18:31 +0300

    updated version

--- src/test/java/com/sauceDemo/runners/FailedTestRunner.java
+++ src/test/java/com/sauceDemo/runners/FailedTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "com/apcrm/step_definitions",
+        glue = "com/sauceDemo/step_definitions",
         features = "@target/rerun.txt"
 )
 
